32e573c760aa44b8cb08214724742ea091625691
Jack Zampolin <jack.zampolin@gmail.com>
2018-05-30 08:38:53 -0700

    First commit

---
+++ stellar-core/entry.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ue
+
+function stellar_core_init_db() {
+  local DB_INITIALIZED="/data/.db-initialized"
+
+  if [ -f $DB_INITIALIZED ]; then
+    echo "core db already initialized. continuing on..."
+    return 0
+  fi
+
+  echo "initializing core db..."
+
+  stellar-core --conf /stellar-core.cfg -newdb
+
+  echo "finished initializing core db"
+
+  touch $DB_INITIALIZED
+}
+
+stellar_core_init_db
+
+exec "$@"
